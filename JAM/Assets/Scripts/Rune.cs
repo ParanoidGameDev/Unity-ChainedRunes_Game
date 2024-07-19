894d18340e3bde166ccfabf6e2a09f2296d9371f
ParanoidGameDev <129700059+ParanoidGameDev@users.noreply.github.com>
2024-07-19 13:23:13 -0600

    Rune setup

---
+++ JAM/Assets/Scripts/Rune.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rune : MonoBehaviour {
+
+    public string runeName;
+    public Sprite runeImg;
+
+    void Start() {
+        
+    }
+
+    void FixedUpdate() {
+        
+    }
+}
---
+++ JAM/Assets/Scripts/_GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _GameManager : MonoBehaviour
+{
+    public List<GameObject> runes;
+
+    // Start is called before the first frame update
+    void Start() {
+        Debug.Log(runes.Count);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

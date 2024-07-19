b8cc29ec30e724dceeb05dfcc514dafb062cfd0f
ParanoidGameDev <129700059+ParanoidGameDev@users.noreply.github.com>
2024-07-19 13:38:22 -0600

    Game manager

--- JAM/Assets/Scripts/_GameManager.cs
+++ JAM/Assets/Scripts/_GameManager.cs
@@ -4,11 +4,33 @@ using UnityEngine;
 
 public class _GameManager : MonoBehaviour
 {
-    public List<GameObject> runes;
+    public List<GameObject> runes; //16
+    public List<GameObject> newRunes; //6
+
+    public List<GameObject> enemies; //3
+
+    public GameObject currentEnemy; //1
 
     // Start is called before the first frame update
-    void Start() {
-        Debug.Log(runes.Count);
+    void Start()
+    {/*
+        //Inicializar enemigo
+        //currentEnemy = Instantiate(enemies[1]);
+        //Generar las runas del enemigo // 3 - 5
+        //currentEnemy.GetComponent<Enemy>().rune;
+        //Rellenar las que resten
+        //nalkjsdjgñlksjg
+
+        //Aleatorizarlas
+        //newRunes.Randomize
+
+
+            //DEBUG
+        foreach (GameObject enemy in enemies)
+        {
+            Instantiate(enemy);
+
+        }*/
     }
 
     // Update is called once per frame

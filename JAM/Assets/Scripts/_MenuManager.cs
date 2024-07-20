782b96173908545c82bc5581f2c52b677f5efe8e
ParanoidGameDev <129700059+ParanoidGameDev@users.noreply.github.com>
2024-07-20 00:06:57 -0600

    Scene transitions

---
+++ JAM/Assets/Scripts/_MenuManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class _MenuManager : MonoBehaviour
+{
+    [SerializeField] private _InputManager input;
+
+    public static bool settings = false;
+    
+    private void LateUpdate() {
+        if (this.input.escape) {
+            if (settings) this.CloseSettings();
+            else if(!settings) this.OpenSettings(); 
+        }
+    }
+
+    public void ChangeScene(string sceneName) {
+        Time.timeScale = 1.0f;
+        if(settings) settings = !settings;
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+
+    public void CloseSettings() {
+        Time.timeScale = 1.0f;
+        SceneManager.UnloadSceneAsync("Pause");
+        settings = !settings;
+    }
+    public void OpenSettings() {
+        for (int i = 0; i < SceneManager.sceneCount; i++) {
+            Scene scene = SceneManager.GetSceneAt(i);
+            if (scene.name == "Pause") {
+                return;
+            }
+        }
+        SceneManager.LoadSceneAsync("Pause", LoadSceneMode.Additive);
+        Time.timeScale = 0.0f;
+        settings = !settings;
+    }
+
+    public void QuitGame() {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
